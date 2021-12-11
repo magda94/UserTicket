3be078ad019175bd80263013781e5e08ef743709
Magda Wojciak <magdapuscizna@gmail.com>
2021-12-11 13:45:16 +0100

    First commit

---
+++ src/main/java/com/example/jpaTrainingBasics/JpaTrainingBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaTrainingBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTrainingBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaTrainingBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpaTrainingBasics/JpaTrainingBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaTrainingBasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTrainingBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
